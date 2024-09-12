bbc2650bbd40bdfbbd0ed071d3e533ae8e526ea3
Lucas Ayala Casanova <71414356+HatCliff@users.noreply.github.com>
2024-09-12 20:04:40 +0000

    Trying to add navbar

---
+++ src/components/navbar.php
@@ -0,0 +1,103 @@
+<?php
+echo '<nav
+        class="navbar navbar-expand-sm navbar-light bg-light"
+    >
+        <div class="container">
+            <a class="navbar-brand" href="#">Bruno Tis</a>
+            <button
+                class="navbar-toggler d-lg-none"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapsibleNavId"
+                aria-controls="collapsibleNavId"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#" aria-current="page"
+                            >Inicio
+                            <span class="visually-hidden">(current)</span></a
+                        >
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a
+                            class="nav-link dropdown-toggle"
+                            href="#"
+                            id="dropdownId"
+                            data-bs-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                            >Autos Nuevos</a
+                        >
+                        <div
+                            class="dropdown-menu"
+                            aria-labelledby="dropdownId"
+                        >
+                            <a class="dropdown-item" href="#"
+                                >SUV</a
+                            >
+                            
+                            <a class="dropdown-item" href="#"
+                                >Sedán</a
+                            >
+                            <a class="dropdown-item" href="#"
+                                >Crossover</a
+                            >
+                            <a class="dropdown-item" href="#"
+                                >Pickup</a
+                            >
+                            <a class="dropdown-item" href="#"
+                                >Furgón</a
+                            >
+
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a
+                            class="nav-link dropdown-toggle"
+                            href="#"
+                            id="dropdownId"
+                            data-bs-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                            >Autos Usados</a
+                        >
+                        <div
+                            class="dropdown-menu"
+                            aria-labelledby="dropdownId"
+                        >
+                            <a class="dropdown-item" href="#"
+                                >Action 1</a
+                            >
+                            <a class="dropdown-item" href="#"
+                                >Action 2</a
+                            >
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        
+                    </li>
+                </ul>
+                <form class="d-flex my-2 my-lg-0">
+                    <input
+                        class="form-control me-sm-2"
+                        type="text"
+                        placeholder="Search"
+                    />
+                    <button
+                        class="btn btn-outline-success my-2 my-sm-0"
+                        type="submit"
+                    >
+                        Search
+                    </button>
+                </form>
+            </div>
+        </div>
+    </nav>';
+    
+?>
+
