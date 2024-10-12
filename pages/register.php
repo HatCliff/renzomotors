88918a92dfd9026ce340b12bcafc957cabddc80f
Natter21 <142517692+Natter21@users.noreply.github.com>
2024-10-12 00:40:54 -0300

    En proceso registrar

---
+++ pages/register.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container pl-5 pr-5 text-center">
+
+        <div class="row justify-content-sm-center">
+
+            <div class="col-lg-6 col-sm-12 px-5 mt-2">
+                <div class="card">
+                    <div class="card-body">
+                        <form>
+                            <div class="row ">
+                                <div class="col-6">
+                                    <div class="form-group mb-3">
+                                        <label for="nombre" class="form-label">Nombre</label>
+                                        <input type="text" name="nombre" class="form-control" id="nombre">
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div class="row ">
+                                <div class="col-6">
+                                    <div class="form-group mb-3">
+                                        <label for="apellido" class="form-label">Apellido</label>
+                                        <input type="text" name="name" class="form-control" id="apellido">
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div class="col-6">
+                                <label for="inputEmail4" class="form-label">Correo</label>
+                                <input type="email" class="form-control" id="inputEmail4">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="inputPassword4" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="inputPassword4">
+                            </div>
+
+
+
+
+                            <div class="col-12 d-flex justify-content-center mb-2">
+                                <button type="submit" class="btn btn-secondary">Sign in</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
