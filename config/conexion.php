985e39d528d1d9188764f95bb9bd872ed934072a
benjiwisc <bcifuentes@ing.ucsc.cl>
2024-11-11 13:31:06 -0300

    cambios de bd

--- config/conexion.php
+++ config/conexion.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $usuario = "root";      
 $contrasena = "";       
-$base_datos = "renzo_motors_prueba";  
+$base_datos = "renzo_motors";  
 
 $conexion = new mysqli($host, $usuario, $contrasena, $base_datos);
 
