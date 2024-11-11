965c8810d56d8284c576efdacf3dc961bf4a2993
Lucas Ayala Casanova <luayalac@outlook.cl>
2024-11-11 18:47:20 -0300

    Cambios en el navbar e index, la idea es que sea un solo archivo index

--- components/navbaruser.php
+++ components/navbaruser.php
@@ -10,7 +10,7 @@ if (session_status() == PHP_SESSION_NONE) {
     <div class="container-fluid">
         <a class="navbar-brand" href='<?php echo $carpetaMain; ?>index.php'>
             <img src="<?php echo $carpetaMain; ?>logo.png" alt="Logo"
-                style="width: 40px; height: 40px; margin-right: 10px; filter: invert(1); ">
+                style="width: 40px; height: 40px;  filter: invert(1); ">
             RenzoMotors
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
@@ -19,9 +19,11 @@ if (session_status() == PHP_SESSION_NONE) {
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
+               <!--------
                 <li class="nav-item">
                     <a href='<?php echo $carpetaMain; ?>index.php' class="nav-link active" aria-current="page">Inicio</a>
                 </li>
+                ---->
             </ul>
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item dropdown">
