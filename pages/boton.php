31c3db66f449f37c912cade43b08eda1aa728152
Natter21 <142517692+Natter21@users.noreply.github.com>
2024-11-27 01:03:50 -0300

    exportacion en proceso

---
+++ pages/boton.php
@@ -0,0 +1,22 @@
+<?php   
+    include('../config/conexion.php');
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exportar Informe</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container text-center mt-5">
+        <h1>Informe de Ventas</h1>
+        <p></p>
+        <a href="../../utils/Exportacion_informe_venta.php" class="btn btn-dark" target="_blank">Exportar en PDF</a>
+    </div>
+</body>
+</html>
